782be15bebe9d8a5691030453f8868b68773686b
OsvaldoMendoza93 <osvaldo.mendoza1249@gmail.com>
2023-10-25 13:11:48 -0600

    resolve error name project to serve the app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'paisesApp'`, () => {
+  it(`should have as title 'countriesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('paisesApp');
+    expect(app.title).toEqual('countriesApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('paisesApp');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('countriesApp');
   });
 });
